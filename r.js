2f2f4978858843a5b21b20351ed62956cd6bd455
Andre <80991812+andreorq@users.noreply.github.com>
2023-06-24 00:41:15 +0100

    Update r.js

--- r.js
+++ r.js
@@ -101,3 +101,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+    if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+    }
+}
+
