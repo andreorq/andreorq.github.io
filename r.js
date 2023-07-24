7c4cd3ed79390115fc77cb270c6366f2deb221eb
Andre <80991812+andreorq@users.noreply.github.com>
2023-07-24 16:09:32 +0100

    Update r.js

--- r.js
+++ r.js
@@ -12,10 +12,8 @@
     })
 
 
-  // Get the button:
 var mybutton = document.getElementById("scrollBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction();
 };
@@ -28,7 +26,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
@@ -39,7 +36,6 @@ mybutton.addEventListener("click", topFunction);
 
 
 
-//copyfoot
 
 function copyToClipboard(element) {
   var $temp = $("<input>");
@@ -66,7 +62,7 @@ function copyToClipboard(element) {
 window.addEventListener('scroll', function() {
   var header = document.querySelector('header');
   var scrollPosition = window.pageYOffset;
-  var moveAmount = scrollPosition / 7; // Adjust the value to control the speed of movement
+  var moveAmount = scrollPosition / 7; 
 
   header.style.backgroundPositionY = moveAmount + 'px';
 });
@@ -74,17 +70,14 @@ window.addEventListener('scroll', function() {
 
 
 
-// Check if the cookie policy has been accepted
 function isCookiePolicyAccepted() {
   return localStorage.getItem('cookieAccepted') === 'true';
 }
 
-// Set the cookie policy as accepted
 function setCookiePolicyAccepted() {
   localStorage.setItem('cookieAccepted', 'true');
 }
 
-// Close the cookie policy message
 function closeCookiePolicy(event) {
   event.preventDefault();
   var cookiePolicy = document.getElementById('cookie-policy');
@@ -92,7 +85,6 @@ function closeCookiePolicy(event) {
   setCookiePolicyAccepted();
 }
 
-// Show the cookie policy message after a delay
 function showCookiePolicyDelayed() {
   var cookiePolicy = document.getElementById('cookie-policy');
   setTimeout(function() {
@@ -100,10 +92,8 @@ function showCookiePolicyDelayed() {
   }, 3000); // 5000 milliseconds = 5 seconds
 }
 
-// Handle the click event on the close link
 document.getElementById('close-cookie').addEventListener('click', closeCookiePolicy);
 
-// Show the cookie policy message only if it has not been accepted after a delay
 document.addEventListener('DOMContentLoaded', function() {
   if (!isCookiePolicyAccepted()) {
     showCookiePolicyDelayed();
